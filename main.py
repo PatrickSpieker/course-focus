c707a6db0084a021863b86675f0e14960a99b342
pspieker <PatrickSpieker@users.noreply.github.com>
2015-10-07 20:05:01 -0700

    Added rough link support

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 # importing:
-# regex, pygraphviz, requests, urllib, argv, and beautiful soup
+# regex, requests, urllib, argv, and beautiful soup
 import re
-import pygraphviz as pgv
 import requests
 import urllib
 from sys import argv
 from bs4 import BeautifulSoup
 import simplejson as json
 
-from pprint import pprint
-
-class Course:
-    """Template for course using data from text file"""
-    def __init__(self, name, course_id, prereqs, off_w):
-        self.name = name
-        self.course_id = course_id
-        self.prereqs = prereqs
-        self.off_w = off_w
-
-
+# picking up command line argument
 dept = str(argv[1])
 
-# empty list for all Course objects
-courses = []
 # empty list for all Course JSON
-course_json_list = []
+nodes = []
+# empty list for course links
+links = []
+
+# Prereq dictionary for link creation
+link_dict = {}
+
 # base url
 url = "http://www.washington.edu/students/crscat/" + dept.lower() + ".html"
 # fetching HTML
@@ -80,9 +73,10 @@ for tag in soup.find_all("a", attrs={"name":re.compile(patt)}):
                 if "or" in i:
                     p = re.compile("[A-Z].[A-Z]{1,3}\s\d\d\d")
                     # append the ENTIRE tuple of options to the prereq list
-                    p_tup = tuple(p.findall(i))
-                    prereqs.append(p_tup)
-                    # the prereq list is tuple-ified at the end of the process
+                    if p.findall(i):
+                        p_tup = tuple(p.findall(i))
+                        prereqs.append(p_tup)
+                        # the prereq list is tuple-ified at the end of the process
                 else:
                     p = re.compile("[A-Z].[A-Z]{1,3}\s\d\d\d")
                     # append EACH ITEM from the list of options
@@ -90,7 +84,10 @@ for tag in soup.find_all("a", attrs={"name":re.compile(patt)}):
                         prereqs.append(j)
 
             # making prereqs immutable via tuple
-            prereqs = tuple(prereqs)
+            if prereqs:
+                prereqs = tuple(prereqs)
+            else:
+                prereqs = tuple()
 
         if child.b:
             # defining string with course info
@@ -98,67 +95,58 @@ for tag in soup.find_all("a", attrs={"name":re.compile(patt)}):
             # finding index of end of course string
             course_str_end = course_str.find("(")-1
             # finding length of course_id
-            id_end = len(dept.lower())+4 # length of space and number
+            id_end = len(dept.lower())+4  # length of space and number
             # defining course id, ex: CSE 143
             course_id = course_str[0:id_end]
             # defining actual name of the course
             name = course_str[8:course_str.find("(")-1]
-            # instantiate object
-        #current_course = Course(name, course_id, prereqs, off_w)
-        #courses.append(current_course)
-        obj = {u"course_id": course_id, u"name": name,
+
+        # creating JSON object to represent current node
+        node = {u"course_id": course_id, u"name": name,
                u"prereqs": prereqs, u"off_w": off_w}
-        course_json_list.append(obj)
+        nodes.append(node)
+
+        # adding link to link dict
+        link_dict[node[u"course_id"]] = nodes.index(node)
+
+        # adding prereqs to node list
+        #for prereq in prereqs:
+         #   if type(prereq) is tuple:
+          #      for act_prereq in prereq:
+           #         if not link_dict[node[act_prereq]]:
+                #        nodes.append({})
+
+
+# for key in link_dict:
+#    print key + ": " + str(link_dict[key])
+for node in nodes:
+    if node["prereqs"]:
+        for prereq in node["prereqs"]:
+            if type(prereq) is tuple:
+                for act_prereq in prereq:
+                    try:
+                        links.append({"source": link_dict[act_prereq],
+                                      "target": link_dict[node["course_id"]]
+                                      })
+                    except KeyError as e:
+                        pass #print e
+            else:
+                try:
+                    links.append({"source": link_dict[prereq],
+                                  "target": link_dict[node["course_id"]],
+                                  })
+                except KeyError as e:
+                    pass #print e
+
+#
+
 
 
 # setting up JSON output file
-json_output = {"nodes": course_json_list,
-               "links": None}
+json_output = {"nodes": nodes,
+               "links": links}
 
 with open("course-data.json", "w") as outfile:
     json.dump(json_output, fp=outfile)
 
-
-# instantiating graph with pgv
-G = pgv.AGraph(directed=True, overlap = False, splines="polyline",
-                nodesep=2.0, sep = +0.25)
-
-
-# connecting courses with prereqs
-for course in courses:
-    # checking for prereqs
-    if course.prereqs:
-        # setting up counter to change color of edge ->
-        # (cont) if there are multiple sets of prereq options
-        sets_option = 0
-
-        for prereq in course.prereqs:
-            # colors for different sets of prereq options
-            colors = ["red", "green", "blue", "purple", "orange", "pink"]
-            # determining whether there are prereq options (in a tuple)
-            if type(prereq) is tuple:
-                # looping through the ACTUAL prereq courses inside tuple
-                for act_prereq in prereq:
-                    # adding edge to graph
-                    G.add_edge(act_prereq, course.course_id)
-                    # formatting edge
-                    e = G.get_edge(act_prereq, course.course_id)
-                    # adding specific color to prereq options
-                    e.attr["color"] = colors[sets_option]
-                    for node in (act_prereq, course.course_id):
-                        n = G.get_node(node)
-                        n.attr["fontsize"] = 8.0
-                # incrementing color option
-                sets_option += 1
-            else:
-                # adding edge to graph
-                G.add_edge(prereq, course.course_id)
-                # formatting edge
-                e = G.get_edge(prereq, course.course_id)
-                e.attr["color"] = "black"
-                # formatting both nodes
-                for node in (prereq, course.course_id):
-                    n = G.get_node(node)
-                    n.attr["fontsize"] = 7.0
-                    
 
