3d6d1b30dea01f6d8e4dd3aa07714f2878cfa2ec
Patrick Spieker <pspieker@cs.washington.edu>
2016-03-21 00:48:58 -0700

    New JS files and dir added

---
+++ js/fdg.js
@@ -0,0 +1,62 @@
+var w = 1080;
+    var h = 960;
+var svg = d3.select("div.graph").append("svg")
+        .attr("width", w)
+        .attr("height", h);
+var force = d3.layout.force()
+        .gravity(.05)
+        .distance(100)
+        .charge(-100)
+        .size([w, h]);
+
+d3.json('ext/force-data.json', function(error, json_data) {
+    if (error) throw error;
+
+    force.nodes(json_data.nodes)
+            .links(json_data.links)
+            .start();
+
+    var node = svg.selectAll("g")
+            .data(json_data.nodes);
+
+    var nodeEnter = node.enter()
+            .append("g")
+            .call(force.drag);
+
+    var circle = nodeEnter.append("circle")
+            .attr("r", 25)
+            .attr("stroke", "black")
+            .attr("fill", "white");
+
+    nodeEnter.append("text")
+            .attr("dx", -12)
+            .attr("font-size", "8px")
+            .text(function (d) {
+                return d["course_id"];
+            });
+
+    var link = svg.selectAll(".link")
+            .data(json_data.links)
+            .enter().append("line")
+            .attr("class", "link");
+
+    force.on("tick", function () {
+        link
+                .attr("x1", function(d) {
+                    return d.source.x;
+                })
+                        .attr("y1", function(d) {
+                            return d.source.y;
+                        })
+                        .attr("x2", function(d) {
+                            return d.target.x;
+                        })
+                        .attr("y2", function(d) {
+                            return d.target.y;
+                        });
+                node.attr("transform", function (d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                });
+
+            });
+});
---
+++ js/jq.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('button.new-dept').click(function() {
+       console.log("Hello world");
+    });
+});
