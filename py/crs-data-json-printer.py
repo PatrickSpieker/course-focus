00e77240d86eb046c24ed10136cd82630de2863d
Patrick Spieker <pspieker@cs.washington.edu>
2016-03-22 15:42:42 -0700

    Added filter-by-dept functionality for the overall print utility

--- py/crs-data-json-printer.py
+++ py/crs-data-json-printer.py
@@ -6,11 +6,15 @@ try:
     file_name = sys.argv[1];
     with open(file_name, "r") as f:
         json_data = json.load(f)
-        for dept in json_data:
-            pprint(dept)
-            pprint(json_data[dept])
-            print "\n"
+        if len(sys.argv) == 3:
+            dept_name = sys.argv[2];  
+            pprint(json_data[dept_name])
+        else:
+            for dept in json_data:
+                pprint(dept)
+                pprint(json_data[dept])
+                print "\n"
             
-except:
-    print "Department name not valid"
+except Exception as e:
+    print e
 
