6258053971ac3642b81e84dfab99d6fec8258387
pspieker <pspieker@cs.washington.edu>
2016-04-27 06:18:57 -0700

    yay end of the night finished push

--- js/jq.js
+++ js/jq.js
@@ -3,10 +3,15 @@
  */
 $(document).ready(function() {
 
-    var slider = $('.bxslider').bxSlider({
+    var prereqSlider = $("#prereq-slider").bxSlider({
         mode: 'horizontal',
         infiniteLoop: true
 
+    });
+
+    var postOptSlider = $("#postopt-slider").bxSlider({
+        mode: 'horizontal',
+        infiniteLoop: true
     });
 
 
@@ -24,8 +29,9 @@ $(document).ready(function() {
 
     $("button.new-course").click(function(e) {
         e.preventDefault();
-        // emptying the slider and the selected course info 
-        slider.empty();
+        // emptying the sliders and the selected course info
+        prereqSlider.empty();
+        postOptSlider.empty();
         $(".selected-info").empty();
 
         var course_id = $("#courses").val();
@@ -34,17 +40,44 @@ $(document).ready(function() {
         $.getJSON("../ext/course-data-uwccs.json", function(data) {
             var course = data[dept][course_id];
             $(".selected-info").append(course["course_info"]);
+
+            /* Dealing with the prereqs */
             if (course["reg_prereqs"].length + course["choice_prereqs"].length == 0) {
-                $(".bxslider").append("<li><p>No prereqs</p></li>");
-                slider.reloadSlider();
+                $("#prereq-slider").append("<li><p>No prereqs</p></li>");
+                prereqSlider.reloadSlider();
             }
             for (var i = 0; i < course["reg_prereqs"].length; i++) {
-                $(".bxslider").append("<li><p>" + course["reg_prereqs"][i] + "</p></li>");
-                slider.reloadSlider();
+                var cid = course["reg_prereqs"][i];
+                var toBeDisplayed = "";
+                if (cid in data[dept]) {
+                    toBeDisplayed = data[dept][cid]["course_info"];
+                } else {
+                    toBeDisplayed = cid;
+                }
+                $("#prereq-slider").append("<li><p>" + toBeDisplayed + "</p></li>");
+                prereqSlider.reloadSlider();
             }
             for (var i = 0; i < course["choice_prereqs"].length; i++) {
-                $(".bxslider").append("<li><p>" + course["choice_prereqs"][i] + "</p></li>");
-                slider.reloadSlider();
+                $("#prereq-slider").append("<li><p>" + course["choice_prereqs"][i] + "</p></li>");
+                prereqSlider.reloadSlider();
+            }
+
+            /* Dealing with the post-options */
+            if (course["is_prereq_for"].length == 0) {
+                $("#postopt-slider").append("<li><p>This class isn't a prerequisite for any classes</p></li>");
+                postOptSlider.reloadSlider();
+
+            }
+            for (var i = 0; i < course["is_prereq_for"].length; i++) {
+                var cid = course["is_prereq_for"][i];
+                var toBeDisplayed = "";
+                if (cid in data[dept]) {
+                    toBeDisplayed = data[dept][cid]["course_info"];
+                } else {
+                    toBeDisplayed = cid;
+                }
+                $("#postopt-slider").append("<li><p>" + toBeDisplayed + "</p></li>");
+                postOptSlider.reloadSlider();
             }
         });
     });
